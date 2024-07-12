947fccaa9e8944e8bd822eb8cbf3e21f348ff475
wasadmin <wasadmin@example.com>
2024-07-12 16:31:57 -0400

    adds api/locations and api/jobs for unique location and jobs

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const connectToDatabase = require('../db');
 const { ObjectId } = require('mongodb');
 // Route to get all employees
 router.get("/all_employees", async (req, res) => {
-    const { client,database, employees } = await connectToDatabase();
+    const { client,database, employees,job } = await connectToDatabase();
     try {
         
         // Query all employees
@@ -41,5 +41,42 @@ router.get("/employee/:id", async (req, res) => {
         
     }
 });
+router.get("/jobs", async (req, res) => {
+    const { client, jobs_collection } = await connectToDatabase();
+    try {
+        // Query all employees
+        const jobs = await jobs_collection.find({}).toArray();
+        
+        res.status(200).send(jobs);
+        console.log("Fetched all jobs:");
+    } catch (error) {
+        console.error("Error fetching employees:", error);
+        res.status(500).send({ message: "An error occurred while fetching jobs" });
+    } finally {
+       
+        await client.close(); // Close the client connection
+        
+    }
+});
+
+router.get("/locations", async (req, res) => {
+    const { client, location_coll } = await connectToDatabase();
+    try {
+        // Query all employees
+        const locations = await location_coll.find({}).toArray();
+        
+        res.status(200).send(locations);
+        console.log("Fetched all locations:");
+    } catch (error) {
+        console.error("Error fetching locations:", error);
+        res.status(500).send({ message: "An error occurred while fetching locations" });
+    } finally {
+       
+        await client.close(); // Close the client connection
+        
+    }
+});
+
+
 
 module.exports = router;
--- db/index.js
+++ db/index.js
@@ -8,8 +8,10 @@ async function connectToDatabase() {
         await client.connect();
         const database = client.db('directory');
         const employees = database.collection('employee_info')
+        const jobs_collection = database.collection('jobs')
+        const location_coll = database.collection('locations')
         
-        return { client, database, employees };
+        return { client, database, employees,jobs_collection, location_coll};
     } catch (error) {
         console.error("Error connecting to the database:", error);
         throw error; // Propagate the error back to the caller if needed
